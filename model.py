321cbfc2909c591ded6b044ae0e0fea2a9e2bc1e
vedha-vikash-pixel <69078828+vedha-vikash-pixel@users.noreply.github.com>
2020-10-31 00:07:02 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,9 +8,7 @@ from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from keras.models import load_model
 
-
 def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
-
     return gen.flow_from_directory(dir,batch_size=batch_size,shuffle=shuffle,color_mode='grayscale',class_mode=class_mode,target_size=target_size)
 
 BS= 32
@@ -21,32 +19,19 @@ SPE= len(train_batch.classes)//BS
 VS = len(valid_batch.classes)//BS
 print(SPE,VS)
 
-
-# img,labels= next(train_batch)
-# print(img.shape)
-
 model = Sequential([
     Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),
     MaxPooling2D(pool_size=(1,1)),
     Conv2D(32,(3,3),activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
-#32 convolution filters used each of size 3x3
-#again
+
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
 
-#64 convolution filters used each of size 3x3
-#choose the best features via pooling
-    
-#randomly turn neurons on and off to improve convergence
     Dropout(0.25),
-#flatten since too many dimensions, we only want a classification output
     Flatten(),
-#fully connected to get all relevant data
     Dense(128, activation='relu'),
-#one more dropout for convergence' sake :) 
     Dropout(0.5),
-#output a softmax to squash the matrix into output probabilities
     Dense(2, activation='softmax')
 ])
 
